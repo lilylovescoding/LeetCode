61247ec5451d0467e2dd26f82f4be11adbf793e8
lily <susieshen828@gmail.com>
2023-05-06 23:59:11 +0800

    add tree

--- src/package111_contest20230430/Solution3.java
+++ src/package111_contest20230430/Solution3.java
@@ -1,8 +1,8 @@
 package package111_contest20230430;
 
-public class Solution3 {
-    public String smallestBeautifulString(String s, int k) {
-        char[] dp = s.toCharArray();
-
-    }
-}
+//public class Solution3 {
+//    public String smallestBeautifulString(String s, int k) {
+//        char[] dp = s.toCharArray();
+//
+//    }
+//}
